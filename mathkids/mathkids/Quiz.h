aef20d7c00921d9efeb9db9220150fe778e4d7b9
alihaider1264 <alihaider1@outlook.com>
2021-11-15 16:04:48 -0500

    structural changes

---
+++ mathkids/mathkids/Quiz.h
@@ -0,0 +1,126 @@
+#ifndef _QUIZ_
+#define _QUIZ_
+
+#include <iostream>
+#include <fstream>
+#include "GradeLevel.h"
+#include "Score.h"
+#include <time.h>
+#include <string>
+#include <vector>
+
+struct Question {
+	int operand1;
+	int operand2;
+	int op;
+};
+
+class Quiz {
+	std::vector<char> ops;
+	char op;
+	int operand1, operand2;
+	GradeLevel level;
+	Score myScore;
+public:
+	Quiz(const int &grade) {
+		srand(time(NULL));
+		
+		//populate operators from operators.dat
+		std::string line;
+		std::ifstream f;
+		f.open("data/operators.dat");
+		if (f.is_open()) {
+			while (f) {
+				std::getline(f, line);
+				ops.push_back((char)line[0]);
+			}
+		}
+		else {
+			std::cout << "Couldn't open data/operators.dat file\n";
+		}
+		//
+
+		op = ' '; operand1 = 0; operand2 = 0;
+		level = GradeLevel(grade);
+		myScore = Score();
+	}
+
+	int calculate();
+	int getNumber();
+	char getOperator();
+	int getMode();
+	Question generateQuestion();
+	bool checkQuestion(Question, int);
+	Score getScore();
+	~Quiz() {}
+};
+
+int Quiz::calculate() {
+
+	switch (op) {
+	case '+': return operand1 + operand2;
+	case '-': return operand1 - operand2;
+	case '*': return operand1 * operand2;
+	case '/': return operand1 / operand2;
+	}
+
+	return 0;
+}
+
+int Quiz::getNumber() {
+	if (level.getGradeLevel() == 1)
+		return rand() % 10 + 0;
+	else if (level.getGradeLevel() == 2)
+		return rand() % 100 + 0;
+
+	return rand() % 10 + 0;
+}
+
+char Quiz::getOperator() {
+	int bound = ops.size();
+
+	if (level.getGradeLevel() == 1 || level.getGradeLevel() == 2) bound = 3;
+	else if (level.getGradeLevel() == 3) bound = 4;
+
+	return ops[rand() % bound + 0];
+}
+
+int Quiz::getMode() {
+	return rand() % 3 + 0;
+}
+
+
+Question Quiz::generateQuestion() {
+	Question Q;
+
+	operand1 = getNumber();
+	op = getOperator();
+	operand2 = getNumber();
+
+	Q.operand1 = operand1;
+	Q.operand2 = operand2;
+	Q.op = op;
+	
+	return Q;
+}
+
+bool Quiz::checkQuestion(Question Q, int result) {
+
+	if (result == calculate()) {
+		myScore.inc_correct();
+		myScore.inc_total();
+		return true;
+	}
+	else {
+		myScore.inc_total();
+		return false;
+	}
+}
+
+Score Quiz::getScore() {
+	return myScore;
+}
+
+
+
+#endif
--- mathkids/mathkids/Source.cpp
+++ mathkids/mathkids/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "GradeLevel.h"
 #include "Score.h"
+#include "Quiz.h"
 #include <time.h>
 
 #define ops_array_size 4
@@ -14,79 +15,35 @@ void printQuestion(int, int, char, int);
 int main() {
 
 	std::cout << "Welcome to Maths!\n";
-	srand(time(NULL));
-	char ops[ops_array_size] = { '+', '-', '*', '/' };
-	char op;
-	int operand1, operand2;
-	GradeLevel level = GradeLevel();
-	Score myScore = Score();
-
 	int grade, no_questions;
-	std::cout << "Enter grade: ";
+	std::cout << "Enter grade (1-5): ";
 	std::cin >> grade;
-	level.setGradeLevel(grade);
+	Quiz myQuiz = Quiz(grade);
 
 	std::cout << "How many questions?: ";
 	std::cin >> no_questions;
 
-	for (int i=0;i<no_questions;i++){
+	Question Q;
+	for (int i = 0; i < no_questions; i++) {
+		Q = myQuiz.generateQuestion();
 
 		int result;
-		operand1 = getNumber(level);
-		char op = getOperator(ops, level);
-		operand2 = getNumber(level);
-		
-		printQuestion(operand1, operand2, op, getMode());
+		printQuestion(Q.operand1, Q.operand2, Q.op, myQuiz.getMode());
 		std::cin >> result;
 
-		if (result == calculate(operand1, operand2, op)){
-			std::cout << "\t\t\tCORRECT!\n";
-			myScore.inc_correct();
-			}
-		else{
+
+		if (myQuiz.checkQuestion(Q, result))
+			std::cout << "\t\t\tCORRECT!\n";		
+		else 
 			std::cout << "\t\t\tWRONG!\n";
-			}
-		
-		myScore.inc_total();
+
 	}
 
+	Score myScore = myQuiz.getScore();
 	std::cout << "Score: " << myScore.getCorrect() << "/" << myScore.getTotal() << " " << myScore.getScore() << "\n";
 }
 
 
-int calculate(int op1, int op2, char op) {
-
-	switch (op) {
-	case '+': return op1 + op2;
-	case '-': return op1 - op2;
-	case '*': return op1 * op2;
-	case '/': return op1 / op2;
-	}
-
-	return 0;
-}
-
-int getNumber(GradeLevel level) {
-	if(level.getGradeLevel() == 1 )
-		return rand() % 10 + 0;
-	else if (level.getGradeLevel() == 2)
-		return rand() % 100 + 0;
-	
-	return rand() % 10 + 0;
-}
-
-char getOperator(char ops[ops_array_size], GradeLevel level) {
-	int bound = ops_array_size;
-
-	if (level.getGradeLevel() == 1 || level.getGradeLevel() == 2) bound = ops_array_size - 2;
-	else if (level.getGradeLevel() == 3) bound = ops_array_size - 1;
-
-	return ops[rand() % bound + 0];
-}
-
-int getMode() {
-	return rand() % 3 + 0;
-}
 
 void printQuestion(int operand1, int operand2, char op, int mode) {
 
